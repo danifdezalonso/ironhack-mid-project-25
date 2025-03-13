ebd4533a89c4693ebc2ba1926d9099f825199bcd
Dani Fernandez <danifdezalonso@gmail.com>
2025-03-13 23:43:59 +0100

    Project section and half of the testimonial

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".navbar");
 
 hamburger.addEventListener("click", () => {
   console.log("clicked");
   if (navMenu.style.display === "block") {
     navMenu.style.display = "none";
+    navBar.style.flexDirection = "row";
   } else {
     navMenu.style.display = "block";
+    navBar.style.flexDirection = "column";
   }
 });
